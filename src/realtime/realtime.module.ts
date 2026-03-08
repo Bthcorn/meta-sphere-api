2bed5b352cfbd071b02a5531c81207f9c2ed4e31
Bthcorn <bowornthat.maize@gmail.com>
2026-03-08 16:43:24 +0700

    fix: refactor and add ws-guard, decorator

--- src/realtime/realtime.module.ts
+++ src/realtime/realtime.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { StateService } from './state.service';
 import { RealtimeGateway } from './realtime.gateway';
+import { WsJwtGuard } from './guards/ws-jwt.guard';
 
 @Module({
-  imports: [JwtModule, ConfigModule],
-  providers: [StateService, RealtimeGateway],
+  imports: [
+    ConfigModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_ACCESS_TOKEN_SECRET'),
+        signOptions: { expiresIn: '1h' },
+      }),
+    }),
+  ],
+  providers: [StateService, RealtimeGateway, WsJwtGuard],
 })
 export class RealtimeModule {}
